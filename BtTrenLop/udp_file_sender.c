705bee74a1a9f0222535ab5f9ce8cda7fccf404e
PDcuong22 <130836467+PDcuong22@users.noreply.github.com>
2023-04-23 22:40:00 +0700

    Add files via upload

---
+++ BtTrenLop/udp_file_sender.c
@@ -0,0 +1,45 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    char *ip_address = argv[1];
+    int portNumber = atoi(argv[2]);
+    char *fileName = argv[3];
+
+    FILE *fptr;
+
+    if ((fptr = fopen(fileName, "r")) == NULL)
+    {
+        printf("Wrong file name!!!\n");
+        return 0;
+    }
+
+    int sender = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip_address);
+    addr.sin_port = htons(portNumber);
+
+    char buf[256];
+
+    int ret = sendto(sender, fileName, strlen(fileName), 0, (struct sockaddr *)&addr, sizeof(addr));
+
+    while (!feof(fptr))
+    {
+        ret = fread(buf, 1, sizeof(buf), fptr);
+        if (ret <= 0)
+            break;
+        ret = sendto(sender, buf, strlen(buf), 0, (struct sockaddr *)&addr,
+                     sizeof(addr));
+    }
+
+    fclose(fptr);
+}
